c3ba8390530e8323feb1381bc5f0b45bd67efa92
fangqk1991 <me@fangqk.com>
2022-03-08 17:25:07 +0800

    v0.1.0: Init Project

---
+++ src/GeneratorCore.ts
@@ -0,0 +1,77 @@
+import { ModelGenerator } from './ModelGenerator'
+import { FCDatabase } from 'fc-sql'
+
+export interface DBConfig {
+  host: string
+  port: number
+  dialect: string
+  database: string
+  username: string
+  password: string
+}
+
+export interface DBModelSchema {
+  generator?: ModelGenerator
+  tableName: string
+  dbProp?: string
+  outputFile: string
+  extFile?: string
+  dbConfig?: DBConfig
+  tmplFile?: string
+  readonly?: boolean
+  reloadOnAdded?: boolean
+  reloadOnUpdated?: boolean
+  primaryKey?: string | string[]
+  readableWhiteList?: string[]
+  readableBlackList?: string[]
+  insertableWhiteList?: string[]
+  forceInsertableWhiteList?: string[]
+  forceModifiableWhiteList?: string[]
+  insertableBlackList?: string[]
+  modifiableWhiteList?: string[]
+  modifiableBlackList?: string[]
+  extendPropertyList?: { name: string; type: string }[]
+  // For AggregateSchema
+  useAggregate?: boolean
+  customColumnDescList?: string[]
+  /**
+   * SQL Column Name => Specific property
+   */
+  specificProps?: { [p: string]: string }
+  keepRawPropName?: boolean
+  uniqueKeyModifiable?: boolean
+  extrasSchemaData?: {
+    [p: string]: string
+  }
+}
+
+export interface ModelColumn {
+  columnName: string
+  columnType: string
+  hasDefValue: boolean
+  defValue: string | number | null
+  propDescription: string
+  nullable: boolean
+  insertable: boolean
+  modifiable: boolean
+  isTimestamp?: boolean
+}
+
+export interface DBReadonlySchema {
+  database: FCDatabase
+  tableName: string
+  readableWhiteList?: string[]
+  readableBlackList?: string[]
+  specificProps?: { [p: string]: string }
+  outputFile?: string
+}
+
+export interface TSMapperProp {
+  propKey: string
+  propAttr: 'string' | 'number'
+}
+
+export interface TSMapper {
+  mapperName: string
+  propList: TSMapperProp[]
+}
